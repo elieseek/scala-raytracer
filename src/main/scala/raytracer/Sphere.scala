ab553d7b0d941dd3a75a2dbacfc74d2bcb366840
elieseek <elie@Elie-PC.localdomain>
2020-05-31 16:50:59 +1000

    removed HitRecord as redundant parameter

--- src/main/scala/raytracer/Sphere.scala
+++ src/main/scala/raytracer/Sphere.scala
@@ -7,8 +7,8 @@ case class Sphere(centre: Vec3, radius: Double, material: Material) extends Hitt
   
   // solves quadratic t^2b.b + 2tb.(A−C) + (A−C).(A−C) − r^2 = 0
   // where ray = Ray(A, b), centre = C
-  def hit(r: Ray, tMin: Double, tMax: Double, record: HitRecord): Option[HitRecord] = {
-    var rec = record
+  def hit(r: Ray, tMin: Double, tMax: Double): Option[HitRecord] = {
+    var rec = HitRecord(Vec3(0,0,0), Vec3(0,0,0), Lambertian(Vec3(0,0,0)), 0.0, false)
     val oc = r.origin - centre
     val a = r.direction.lengthSquared
     val halfB = dot(oc, r.direction)
@@ -20,7 +20,6 @@ case class Sphere(centre: Vec3, radius: Double, material: Material) extends Hitt
       if (temp < tMax && temp > tMin) {
         rec.t = temp
         rec.p = r.at(rec.t)
-        rec.normal = (rec.p - centre) / radius
         var outwardNormal = (rec.p - centre) / radius
         rec.setFaceNormal(r, outwardNormal)
         rec.mat = material
@@ -30,7 +29,6 @@ case class Sphere(centre: Vec3, radius: Double, material: Material) extends Hitt
         if (temp < tMax && temp > tMin) {
           rec.t = temp
           rec.p = r.at(rec.t)
-          rec.normal = (rec.p - centre) / radius
           var outwardNormal = (rec.p - centre) / radius
           rec.setFaceNormal(r, outwardNormal)
           rec.mat = material
