bfad6c51e171ab276e804627ee0cb296e255aa8f
elieseek <elie@Elie-PC.localdomain>
2020-06-03 21:47:13 +1000

    added checkered texture

--- src/main/scala/raytracer/Texture.scala
+++ src/main/scala/raytracer/Texture.scala
@@ -1,14 +1,24 @@
 package raytracer
 
+import scala.math.sin
+
 trait Texture {
   def value(u: Double, v: Double, p: Vec3): Vec3
 }
 
 case class SolidColour(colour: Vec3) extends Texture {
-  def this(red: Double, green: Double, blue: Double) = this(Vec3(red, green, blue))
   def value(u: Double, v: Double, p: Vec3) = colour
 }
 
 object SolidColour {
   def apply(red: Double, green: Double, blue: Double) = new SolidColour(Vec3(red, green, blue))
+
+  def apply(colour: Vec3) = new SolidColour(colour)
+}
+
+case class CheckerTexture(even: Texture, odd: Texture) extends Texture {
+  def value(u: Double, v: Double, p: Vec3) = {
+    val sines = sin(10*p.x)*sin(10*p.y)*sin(10*p.z)
+    if (sines < 0) odd.value(u, v, p) else even.value(u, v, p)
+  }
 }
